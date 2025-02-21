b5611145e90fdfdbf348d6c486a896f2ec8adf80
kdreddy2582 <kdreddy582@gmail.com>
2025-02-22 01:19:51 +0530

    navbar creation

---
+++ src/app/pages/path/path.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-path',
+  imports: [],
+  templateUrl: './path.component.html',
+  styleUrl: './path.component.scss'
+})
+export class PathComponent {
+
+}
