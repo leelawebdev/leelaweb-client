d06cab0446f6056dab98475ac89cf73f695e63d2
kdreddy2582 <kdreddy582@gmail.com>
2025-02-24 23:47:31 +0530

    Authentication forms

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,15 @@ import { HomeComponent } from './pages/home/home.component';
 import { TopicsComponent } from './pages/topics/topics.component';
 import { SeriesComponent } from './pages/series/series.component';
 import { PathComponent } from './pages/path/path.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'topics', component: TopicsComponent },
   { path: 'series', component: SeriesComponent },
   { path: 'path', component: PathComponent},
+  { path: 'login', component: LoginComponent},
+  { path: 'register', component: RegisterComponent},
   { path: 'home', component: HomeComponent },
 ];
