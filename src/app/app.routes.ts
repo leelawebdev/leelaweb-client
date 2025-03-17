39194902f9f3e60bcffb0edcce884f252a5c6ef2
LeelaNarasimha <leelanarasimha@teamvelocitymarketing.com>
2025-03-17 18:55:00 +0530

    Add admin routing and components for admin dashboard and series

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -11,9 +11,14 @@ export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'topics', component: TopicsComponent },
   { path: 'series', component: SeriesComponent },
-  { path: 'path', component: PathComponent},
-  { path: 'login', component: LoginComponent},
-  { path: 'register', component: RegisterComponent},
-  { path: 'reset-password', component: ResetPasswordComponent},
+  { path: 'path', component: PathComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'reset-password', component: ResetPasswordComponent },
   { path: 'home', component: HomeComponent },
+  {
+    path: 'admin',
+    loadChildren: () =>
+      import('./pages/admin/admin.routes').then((m) => m.adminRoutes),
+  },
 ];
