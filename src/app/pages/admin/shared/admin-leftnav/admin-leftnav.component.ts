33da0015cee5ea65160ab956f41d78b720f71fac
kdreddy2582 <kdreddy582@gmail.com>
2025-03-22 19:26:25 +0530

    added changes

--- src/app/pages/admin/shared/admin-leftnav/admin-leftnav.component.ts
+++ src/app/pages/admin/shared/admin-leftnav/admin-leftnav.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import {  RouterOutlet, RouterLink } from '@angular/router';
+import { AdminTopnavComponent } from '../admin-topnav/admin-topnav.component';
 
 @Component({
   selector: 'app-admin-leftnav',
-  imports: [RouterOutlet, RouterLink],
+  imports: [RouterOutlet, RouterLink, AdminTopnavComponent],
   templateUrl: './admin-leftnav.component.html',
   styleUrl: './admin-leftnav.component.scss'
 })
